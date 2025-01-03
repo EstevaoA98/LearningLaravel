670ac972e87455c9553b38ebcab9f64240780740
Estevao Miranda <estevao.a98@gmail.com>
2025-01-03 16:55:29 -0300

    learning

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = 'Estevao';
+    $idade = 26;
+    $profissao = 'Desenvolvedor Web';
+
+    $arr = ['1', '2', '3', '4', '5', '6', '7'];
+    $nomes = ['joao','maria','jose','esveraldo','maria aparecida'];
+    return view('welcome', [
+        'nome' => $nome,
+        'idade' => $idade,
+        'profissao' => $profissao,
+        'arr' => $arr,
+        'nomes'=> $nomes
+    ]);
 });
